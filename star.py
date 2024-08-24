36832fe84a1f2461918b8bb088224f5889a21f86
seulgisohn <156396769+seulgisohn@users.noreply.github.com>
2024-08-24 13:20:03 +0900

    Add files via upload

---
+++ star.py
@@ -0,0 +1,17 @@
+import turtle as t
+import random
+color = ("red", "orange", "blue", "green", "white", "yellow", "indigo", "pink")
+t.bgcolor("black")
+t.speed(0)
+for x in range(20):
+    t.up()
+    t.goto(random.randint(-300,300), random.randint(-300,300))
+    t.down()
+    #별그리기
+    t.color(random.choice(color))
+    t.begin_fill()
+    star_size = random.randint(10,30)
+    for i in range(5):
+        t.forward(star_size)
+        t.left(144)
+    t.end_fill()
